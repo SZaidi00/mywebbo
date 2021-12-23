0a69a7e70e34d17f02f3f4f46cf9de228b40b04f
Shumail Zaidi <43276288+SZaidi00@users.noreply.github.com>
2021-12-22 19:28:44 -0500

    website addition

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,15 +2,15 @@ const Navbar = () => {
     return ( 
         <nav className="navbar">
             
-            <a href="shubacca.xyz"> Shumail's Underworld</a>
+            <a href="www.shubacca.xyz"> Shumail's Underworld</a>
 
             <div className="links">
                 
-                <a href="/create" >About me</a>
-                <a href="/create" >Undergrad</a>
-                <a href="/create" >Hobbies</a>
-                <a href="/create" >Projects</a>
-                <a href="/create" >Fun</a>
+                <a href="www.shubacca.xyz/Aboutme" >About me</a>
+                <a href="www.shubacca.xyz/Undergrad" >Undergrad</a>
+                <a href="www.shubacca.xyz/Hobbies" >Hobbies</a>
+                <a href="www.shubacca.xyz/Projects" >Projects</a>
+                <a href="www.shubacca.xyz/Fun" >Fun</a>
                 <a href="https://www.google.com/" >If you need Google :) </a>
             </div>
 
