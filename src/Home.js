2163b81428f82dffc29fc626dc7132c90c2eaa5a
Shumail Zaidi <43276288+SZaidi00@users.noreply.github.com>
2021-12-22 19:01:04 -0500

    Work in progress

---
+++ src/Home.js
@@ -0,0 +1,16 @@
+const Home = () => {
+    return ( 
+
+
+<div className="Home">
+    <h2>Hello, welcome to the world of Shumail!</h2>
+
+    <div className="BodyHome" > 
+    <p>Whats up punks </p>
+    </div> 
+</div>
+
+     );
+}
+ 
+export default Home ;
---
+++ src/Navbar.js
@@ -0,0 +1,25 @@
+const Navbar = () => {
+    return ( 
+        <nav className="navbar">
+            
+            <a href="shubacca.xyz"> Shumail's Underworld</a>
+
+            <div className="links">
+                
+                <a href="/create" >About me</a>
+                <a href="/create" >Undergrad</a>
+                <a href="/create" >Hobbies</a>
+                <a href="/create" >Projects</a>
+                <a href="/create" >Fun</a>
+                <a href="https://www.google.com/" >If you need Google :) </a>
+            </div>
+
+
+        </nav>
+
+     );
+}
+ 
+export default Navbar;
+
+
