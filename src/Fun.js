5fed8ada5133ddd5af6256c378a94f19c9cf69b0
Shumail Zaidi <43276288+SZaidi00@users.noreply.github.com>
2021-12-27 14:20:40 -0500

    test

--- src/Fun.js
+++ src/Fun.js
@@ -1,6 +1,10 @@
+import Obi from './pictures/hello-there.gif'
+
 const Fun = () => {
     return ( 
         <div className="FUNtime">
+            <img src={Obi} alt="A" />
+            <h2>Looks like you came to have some fun or learn about fun</h2>
 
         </div>
      );
